950c483062fb07e6e0af12364a1ef6127a7fa636
bdistefanoo <bdistefano@mail.austral.edu.ar>
2025-09-08 19:37:43 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -4,3 +4,10 @@ def slice_simple():
     print(texto[2:5].lower())
     print(texto[0:4].lower() + texto[-3:].lower())
 slice_simple()
+    
+    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+
+
+# Para verificar este ejercicio ejecutar el comando
+# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
